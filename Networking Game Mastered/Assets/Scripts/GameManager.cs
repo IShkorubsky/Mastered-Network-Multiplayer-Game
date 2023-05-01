4625475842429a445179d895d98f05444566e06b
IShkorubsky <illyashkorubsky@hotmail.com>
2023-05-01 23:24:07 +0100

    minor changes

--- Networking Game Mastered/Assets/Scripts/GameManager.cs
+++ Networking Game Mastered/Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 using System.IO;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
     private PhotonView PV;
     [SerializeField] private GameObject playerPrefab;
+    private Player[] allPlayers;
+    private int myNumberInTheRoom;
     [SerializeField] private Transform[] playerSpawnPositions;
 
     public void StartGame()
     {
         PV = GetComponent<PhotonView>();
+        allPlayers = PhotonNetwork.PlayerList;
+        foreach (var player in allPlayers)
+        {
+            if (player != PhotonNetwork.LocalPlayer)
+            {
+                myNumberInTheRoom++;
+            }
+        }
         int spawnPicker = Random.Range(0, playerSpawnPositions.Length);
         if (PV.IsMine)
         {
